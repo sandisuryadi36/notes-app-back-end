2b2390feef43ad59c6c7333db6874bbd3339f4aa
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-12-18 20:15:05 +0700

    change service

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Hapi = require('@hapi/hapi')
 const notes = require('./api/notes')
-const NoteService = require('./services/inMemory/noteService')
+const NoteService = require('./services/postgres/noteService')
 const NotesValidator = require('./validator/notes')
 
 const init = async () => {
